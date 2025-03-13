6ada8f142841066509d7783b840b2a8ae0189ef0
Kanaga Jayasankar <kanagasavithri@gmail.com>
2025-03-13 21:00:12 +0000

    Add files via upload

---
+++ Voting_System/src/main/java/india21/JAXRSConfiguration.java
@@ -0,0 +1,12 @@
+package india21;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures JAX-RS for the application.
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+    
+}
